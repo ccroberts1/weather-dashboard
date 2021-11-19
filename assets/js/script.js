f41d03404da9c75932eae65bda14abad63893913
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2021-11-18 20:19:39 -0700

    refined .then to fill out information on card for current day weather info

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@
 var pTemp = document.querySelector("#temp");
 var pWind = document.querySelector("#wind");
 var pHumidity = document.querySelector("#humidity");
-var pUV = document.querySelector("#uvIndex");
 var currentCard = document.querySelector("#currentTitle");
+var pUV = document.querySelector("#uvIndex");
 
 //FUNCTION DECLARATIONS
 //Fetch function to retrieve JSON object from Geocoding API
@@ -31,11 +31,51 @@ function cityInput(event) {
         })
         .then(function (data) {
           console.log(data);
+
+          //Adds city name to card title
           currentCard.append(cityName);
+
+          //Retrieves date for current day card
+          var date = new Date(data.current.dt * 1000);
+          var currentDate = ` (${
+            date.getMonth() + 1
+          }/${date.getDate()}/${date.getFullYear()})`;
+          currentCard.append(currentDate);
+
+          //Adds weather icon to card title
+          var icon = data.current.weather[0].icon;
+          var weatherIcon = document.createElement("img");
+          weatherIcon.setAttribute(
+            "src",
+            `http://openweathermap.org/img/wn/${icon}.png`
+          );
+          currentCard.append(weatherIcon);
+
+          //Adds Temp, Wind, Humidity and UV Index fields
           pTemp.append(`Temp: ${data.current.temp} °F`);
           pWind.append(`Wind: ${data.current.wind_speed} MPH`);
           pHumidity.append(`Humidity: ${data.current.humidity}%`);
-          pUV.append(`UV Index: ${data.current.uvi}`);
+          pUV.append(`UV Index:`);
+
+          //This creates a span to house the number for the UV Index
+          var spanUVNum = document.createElement("span");
+          pUV.append(spanUVNum);
+          var uvNum = data.current.uvi;
+
+          //This determines the background-color for the UV Index
+          if (uvNum <= 2) {
+            spanUVNum.style.backgroundColor = "green";
+          } else if (uvNum > 2 && uvNum <= 5) {
+            spanUVNum.style.backgroundColor = "yellow";
+          } else if (uvNum > 5 && uvNum < 8) {
+            spanUVNum.style.backgroundColor = "orange";
+          } else if (uvNum > 7 && uvNum < 11) {
+            spanUVNum.style.backgroundColor = "red";
+          } else {
+            spanUVNum.style.backgroundColor = "purple";
+          }
+          spanUVNum.style.color = "white";
+          spanUVNum.append(uvNum);
         });
     });
 }
